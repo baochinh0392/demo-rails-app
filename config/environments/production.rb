f92542c0c14623a44cd5b1934a9eaf9da98947dd
Bao Chinh <baochinh0392@gmail.com>
2021-07-24 11:03:49 +0700

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "demo-rails-chinh.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['EMAIL_USER'],
